7f84e06487f577010cd7ec78bd10423d7fc2c630
Yunus Abdul Hanif, S.Kom <42384452+siunus@users.noreply.github.com>
2018-10-05 12:25:59 +0700

    Server to send some message or data.

--- server.php
+++ server.php
@@ -2,18 +2,16 @@
   require __DIR__ . '/vendor/autoload.php';
 
   $options = array(
-    'cluster' => 'ap1',
+    'cluster' => 'YOUR CLUSTER',
     'useTLS' => true
   );
   $pusher = new Pusher\Pusher(
-    '3b6f84a37f1ee2e48c7a',
-    '0f9db8f8f007b83d43b4',
-    '611552',
+    'YOUR API KEY',
+    'YOUR API SECRET',
+    'YOUR APP ID',
     $options
   );
 
-  $data['status'] = 'sukses';
-  $data['username'][1] = 'siunus';
-  $data['username'][2] = 'nendah';
+  $data['message'] = 'hello world';
   $pusher->trigger('my-channel', 'my-event', $data);
 ?>
